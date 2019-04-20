7ebfc050f1f0aa164d511e960419d171a5f5757e
ieprst <letrale@mail.ru>
2019-04-20 19:22:47 +0300

    tulip: Reorder properties like CAF
    Change-Id: Ibfd4d5ea110d6cb8f38516b7d4469ad5ec415303

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,97 +1,71 @@
-# Audio
 PRODUCT_PROPERTY_OVERRIDES +=  \
-    af.fast_track_multiplier=1  \
-    audio.deep_buffer.media=true  \
-    audio.offload.min.duration.secs=30  \
-    audio.offload.video=true  \
-    persist.dirac.acs.controller=qem  \
-    persist.dirac.acs.ignore_error=1  \
-    persist.dirac.acs.storeSettings=1  \
-    persist.vendor.audio.fluence.speaker=true  \
-    persist.vendor.audio.fluence.voicecall=true  \
-    persist.vendor.audio.fluence.voicerec=false  \
-    persist.vendor.audio.hifi.int_codec=true  \
-    persist.vendor.audio.hw.binder.size_kbyte=1024  \
-    persist.vendor.audio.ras.enabled=false  \
-    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac  \
-    ro.af.client_heap_size_kbyte=7168  \
-    ro.audio.soundfx.dirac=true  \
-    ro.config.media_vol_default=10  \
-    ro.config.media_vol_steps=25 \
-    ro.config.vc_call_vol_steps=7 \
-    ro.vendor.audio.sdk.fluencetype=fluence  \
-    ro.vendor.audio.sdk.ssr=false  \
-    ro.vendor.audio.sos=true  \
-    vendor.audio.dolby.ds2.enabled=false  \
-    vendor.audio.dolby.ds2.hardbypass=false  \
-    vendor.audio.flac.sw.decoder.24bit=true  \
-    vendor.audio.hw.aac.encoder=true  \
-    vendor.audio.noisy.broadcast.delay=600  \
-    vendor.audio.offload.buffer.size.kb=64  \
-    vendor.audio.offload.gapless.enabled=true  \
-    vendor.audio.offload.multiaac.enable=true  \
-    vendor.audio.offload.multiple.enabled=false  \
-    vendor.audio.offload.passthrough=false  \
-    vendor.audio.offload.pstimeout.secs=3  \
-    vendor.audio.offload.track.enable=false  \
-    vendor.audio.parser.ip.buffer.size=262144  \
-    vendor.audio.safx.pbe.enabled=true  \
-    vendor.audio.tunnel.encode=false  \
-    vendor.audio.use.sw.alac.decoder=true  \
-    vendor.audio.use.sw.ape.decoder=true  \
-    vendor.audio_hal.period_size=192  \
-    vendor.voice.path.for.pcm.voip=true
-
-# Bluetooth
-PRODUCT_PROPERTY_OVERRIDES +=  \
-    bt.max.hfpclient.connections=1  \
-    persist.bt.a2dp.aac_disable=true  \
-    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxhd-aac  \
-    persist.vendor.btstack.enable.splita2dp=true  \
-    ro.bluetooth.emb_wp_mode=true  \
-    ro.bluetooth.wipower=true  \
-    vendor.bluetooth.soc=cherokee  \
-    vendor.qcom.bluetooth.soc=cherokee
-
-# Camera
-PRODUCT_PROPERTY_OVERRIDES +=  \
-    camera.aux.packagelist=org.codeaurora.snapcam  \
-    persist.vendor.camera.HAL3.enabled=1  \
-    persist.vendor.camera.depth.focus.cb=0  \
-    persist.vendor.camera.exif.make=Xiaomi  \
-    persist.vendor.camera.exif.model=RedmiNote6Pro  \
-    persist.vendor.camera.expose.aux=1  \
-    persist.vendor.camera.fdvideo=1  \
-    persist.vendor.camera.isp.clock.optmz=0  \
-    persist.vendor.camera.isp.turbo=1  \
-    persist.vendor.camera.linkpreview=0  \
-    persist.vendor.camera.ltm.overlap=13  \
-    persist.vendor.camera.preview.ubwc=0  \
-    persist.vendor.camera.stats.test=0  \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam
-
-# Charging maximum voltage
-PRODUCT_PROPERTY_OVERRIDES +=  \
-    persist.chg.max_volt_mv=9000
-
-# CNE and DPM
-PRODUCT_PROPERTY_OVERRIDES +=  \
-    persist.vendor.cne.feature=1  \
-    persist.vendor.dpm.feature=1  \
-    persist.vendor.dpm.loglevel=0
-
-# Dalvik overrides
-PRODUCT_PROPERTY_OVERRIDES +=  \
-    dalvik.vm.heapgrowthlimit=256m  \
-    dalvik.vm.heapmaxfree=8m  \
-    dalvik.vm.heapminfree=512k  \
-    dalvik.vm.heapsize=512m  \
-    dalvik.vm.heapstartsize=8m  \
-    dalvik.vm.heaptargetutilization=0.75  \
-    ro.dalvik.vm.native.bridge=0
-
-# DRM
-PRODUCT_PROPERTY_OVERRIDES +=  \
+    persist.sys.job_delay=true \
+    persist.sys.mcd_config_file=/system/etc/mcd_default.conf \
+    vendor.video.disable.ubwc=1 \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m \
+    vendor.hw.fm.init=0 \
+    persist.radio.multisim.config=dsds \
+    persist.vendor.qcomsysd.enabled=1 \
+    keyguard.no_require_sim=true \
+    ro.com.android.dataroaming=false \
+    ro.dalvik.vm.native.bridge=0 \
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.sib16_support=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.backup.ntpServer=0.pool.ntp.org \
+    sys.vendor.shutdown.waittime=500 \
+    ro.build.shutdown_timeout=0 \
+    ro.frp.pst=/dev/block/bootdevice/by-name/frp \
+    ro.opengles.version=196610 \
+    vendor.qcom.bluetooth.soc=cherokee \
+    af.fast_track_multiplier=1 \
+    vendor.audio_hal.period_size=192 \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.audio.fluence.speaker=true \
+    vendor.audio.tunnel.encode=false \
+    persist.vendor.audio.ras.enabled=false \
+    vendor.audio.offload.buffer.size.kb=64 \
+    audio.offload.min.duration.secs=30 \
+    audio.offload.video=true \
+    vendor.audio.offload.track.enable=false \
+    audio.deep_buffer.media=true \
+    vendor.voice.path.for.pcm.voip=true \
+    vendor.audio.offload.multiaac.enable=true \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.passthrough=false \
+    ro.vendor.audio.sdk.ssr=false \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.safx.pbe.enabled=true \
+    vendor.audio.parser.ip.buffer.size=262144 \
+    vendor.audio.flac.sw.decoder.24bit=true \
+    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
+    vendor.audio.use.sw.alac.decoder=true \
+    vendor.audio.use.sw.ape.decoder=true \
+    vendor.audio.hw.aac.encoder=true \
+    vendor.fm.a2dp.conc.disabled=true \
+    vendor.audio.noisy.broadcast.delay=600 \
+    persist.vendor.audio.hifi.int_codec=true \
+    vendor.audio.offload.pstimeout.secs=3 \
+    ro.af.client_heap_size_kbyte=7168 \
+    persist.vendor.audio.hw.binder.size_kbyte=1024 \
+    ro.vendor.audio.sos=true \
+    ro.vendor.audio.soundtrigger=nuance \
+    ro.vendor.audio.soundtrigger.lowpower=false \
+    ro.vendor.sensors.facing=false \
+    ro.vendor.sensors.cmc=false \
+    ro.vendor.sdk.sensors.gestures=false \
+    sys.autosuspend.timeout=500000 \
     drm.service.enabled=true
 
 # Enable stm-events
