6c52138346a6f61b2dccc7b777f38bf16f6e15a2
ieprst <letrale@mail.ru>
2019-05-11 08:17:34 +0300

    tulip: update bdroid_buildcfg.h

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,35 +21,16 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-
-#include <cutils/properties.h>
-#include <string.h>
-
-static inline const char* BtmGetDefaultName()
-{
-    char product_model[PROPERTY_VALUE_MAX];
-    property_get("ro.product.model", product_model, "");
-
-    if (strstr(product_model, "Redmi Note 5"))
-        return "Redmi Note 5";
-    if (strstr(product_model, "Redmi Note 5 Pro"))
-        return "Redmi Note 5 Pro";
-
-    // Fallback to ro.product.model
-    return "";
-}
-#undef PROPERTY_VALUE_MAX
-
-#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
+#define BTM_DEF_LOCAL_NAME   "Redmi Note 6 Pro"
 #define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    16
+#define MAX_L2CAP_CHANNELS    20
 #define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
 
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
 #define AVDT_NUM_SEPS 12
-
+#undef PROPERTY_VALUE_MAX
 #endif
